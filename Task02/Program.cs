598f5f6a13e87de2af5b8c686020297cffe4b646
Evgeniy <lnf_55@mail.ru>
2023-01-13 17:31:28 +0300

    правка 2 задания

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -3,14 +3,8 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
-Console.WriteLine("Ввидите два числа");
+Console.WriteLine("Ввидите два числа :");
 int number1 = Convert.ToInt32(Console.ReadLine());
 int number2 = Convert.ToInt32(Console.ReadLine());
-if (number1 > number2)
-{
-    Console.WriteLine($"max = {number1} ");
-}
-else
-{
-    Console.WriteLine($"max = {number2} ");
-}
+
+Console.WriteLine(number1 > number2 ? $"max = {number1} " : $"max = {number2} " );
