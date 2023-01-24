d0ac116b697800d79ede374aa6daadac74a6b4b2
Evgeniy <lnf_55@mail.ru>
2023-01-24 15:52:53 +0300

    исправление в задаче 25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -2,23 +2,28 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-double RaisingAinB (double num1,double num2)
+double RaisingAPowerB(double num1, int num2)
 {
     double raising = 1;
     for (int i = 0; i < num2; i++)
     {
         raising *= num1;
     }
-    return raising;
+    return Math.Round(raising,2,MidpointRounding.ToZero);
 }
 
 Console.WriteLine("Введите два числа:");
+
 Console.Write("A:");
 double numberA = Convert.ToDouble(Console.ReadLine());
+
 Console.Write("B:");
-double numberB = Math.Round(Convert.ToDouble(Console.ReadLine()));
-Console.WriteLine(numberB);
+int numberB = Convert.ToInt32(Console.ReadLine());
 
 
-double raising = RaisingAinB(numberA,numberB);
-Console.WriteLine($"{numberA}^{numberB} = {raising}");
+if (numberB <= 0) Console.WriteLine("Некорректный ввод числа B");
+else
+{
+    double raising = RaisingAPowerB(numberA, numberB);
+    Console.WriteLine($"{numberA}^{numberB} = {raising}");
+}
