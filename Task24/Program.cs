6dbbd6b6db2ea6ff5493538e9737788e4e32c1b9
Evgeniy <lnf_55@mail.ru>
2023-01-24 12:44:30 +0300

    добавили задачи 24.26.26.30

---
+++ Task24/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 24: Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+int SumNumber(int num)
+{
+    int sum = 0;
+    for (int i = 0; i <= num; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+Console.Write("Введите целое положительное число:");
+int numb = Convert.ToInt32(Console.ReadLine());
+
+if (numb > 0)
+{
+    int sumNumber = SumNumber(numb);
+    Console.WriteLine($"Сумма чисел от 1 до {numb} = {sumNumber}");
+}
+else Console.WriteLine($"Введено некорректное значение");
+
+
+
+
---
+++ Task26/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+
+int Sumnum(int num){
+    int count = 0;
+    if (num < 0) num = num * -1;
+    while (num > 0)
+    {
+        num = num / 10;
+        count ++;
+    }
+    return count;
+}
+
+Console.Write("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumNumber = Sumnum(number);
+Console.WriteLine($"Количество цифр в числе: {number} -> {sumNumber}");
+
