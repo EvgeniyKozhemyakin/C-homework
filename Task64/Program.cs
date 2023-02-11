a070e7b5a321f73c44b8a201bb0ac302612242b5
Evgeniy <lnf_55@mail.ru>
2023-02-11 18:41:38 +0300

    исправил решение для задачи 64.66.68

---
+++ Task64/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int PrintNaturalNumbers(int num)
+{
+    if (num == 1) Console.Write($"{num}");
+    else Console.Write($"{num}, ");
+    if (num <= 1) return 0;
+    return PrintNaturalNumbers(num - 1);
+}
+
+Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    Console.Write($"N = {number} -> ");
+    PrintNaturalNumbers(number);
+}
+else Console.Write("Вы ввели не натуральное число !");
+
+
+
---
+++ Task66/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+//5      //3
+int SumNaturalDigits(int num1, int num2)
+{
+
+    if (num2 == num1) return num1;
+    if (num2 < num1) return SumNaturalDigits(num2, num1);
+    return num1 += SumNaturalDigits(num1 + 1, num2);
+}
+
+Console.Write("Введите значение M:");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N:");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+if (numberM < 1 || numberM < 1) Console.WriteLine("Введите натуральные числа!");
+else
+{
+    int sumNaturalsDigits = SumNaturalDigits(numberM, numberN);
+    Console.WriteLine(sumNaturalsDigits);
+}
